9add4d23d127ecd84ab15b9ed5e738983a3732ae
Gowrishankar Janakiraman <gowrishankarjanakiraman@GowrishnkarsMBP.hsd1.ca.comcast.net>
2022-12-03 23:09:22 -0800

    class definitions and functions in progress

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 console.log("loaded");
 
+//============== CLASS DEFINITION ============================
+
+class Gameboard {
+  constructor(numTile) {
+    this.board = [
+      [undefined, undefined, undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined, undefined, undefined],
+      [undefined, undefined, undefined, undefined, undefined, undefined],
+    ];
+    this.numberRoll = [2, 4, 8, 16, 32, 64, 128];
+    this.numTile = numTile;
+    this.isGameRunning = false;
+    this.startCol = 3;
+  }
+
+  randomNum() {
+    console.log(
+      "Inside the randomNum fn. The Number roll is " + this.numberRoll
+    );
+    for (let i = 0; i < 5; i++) {
+      const getRandomNumberRoll = Math.floor(
+        Math.random() * this.numberRoll.length
+      );
+      const numTile = this.numberRoll[getRandomNumberRoll];
+      console.log("NumTile is " + numTile);
+      return numTile;
+    }
+  }
+
+  setInterval() {
+    console.log("Inside the Set Interval function");
+    this.randomNum(), 1000;
+  }
+
+  startGame() {
+    console.log("Inside hte startGame function");
+    this.isGameRunning = true;
+    console.log("Value of game running is " + this.isGameRunning);
+    this.setInterval();
+  }
+}
+
+const samp = new Gameboard();
+samp.startGame();
+console.log("Here I am");
+
 /* ========= PSUEDO CODE ===========
 
 High-level steps to achieve before writing the pseudo code:
@@ -7,110 +53,67 @@ High-level steps to achieve before writing the pseudo code:
 1. Identify the numbers in scope for the game.
 2. Identify the numbers that will be in the loop for user selection.
 3. Generate a random number for drop.
-3. Complete proof of concept for one number tile getting to the base.
-4. Complete proof of concept for a different tile settling on top of another tile.
-5. Complete proof of concept for two tiles of the same number (horizontal drop) getting added to a single tile.
+3. Complete the concept for one number tile getting to the base. (require moving animation, require onclick catch)
+4. Complete the concept for a tile with a different number settling on top of tile with another number.
+5. Complete concept for two tiles of the same number (horizontal drop) getting added to a single tile.
 6. Extend 5 to add two tiles having same number, but vertically aligned.
-7. Adjust the alignment for each calculation.
+7. Adjust the alignment for each calculation (if tile drops right, end state will be updated to right and vice versa).
 8. Update the Points earned for every merge.
+9. continue random number generation and on click of a column, send it down to column of click.
+10. Reset - erase points, reload the game.
 
 
 
 
 */
 
-const numberRoll = [2, 4, 8, 16, 32, 64, 128];
-console.log("number roll is " + numberRoll);
+// Initialize the class for a number with the below details
 
-const numbArr = [
-  {
-    nbr: 2,
-    fromClr: "#e9d8a6",
-    tgtClr: "#ee9b00",
-  },
-  {
-    nbr: 4,
-    fromClr: "#ee9b00",
-    tgtClr: "#94d2bd",
-  },
-  {
-    nbr: 8,
-    fromClr: "#94d2bd",
-    tgtClr: "#ca6702",
-  },
-  {
-    nbr: 16,
-    fromClr: "#ca6702",
-    tgtClr: "#0a9396",
-  },
-  {
-    nbr: 32,
-    fromClr: "#0a9396",
-    tgtClr: "#bb3e03",
-  },
-  {
-    nbr: 64,
-    fromClr: "#bb3e03",
-    tgtClr: "#005f73",
-  },
-  {
-    nbr: 128,
-    fromClr: "#005f73",
-    tgtClr: "#ae2012",
-  },
-  {
-    nbr: 256,
-    fromClr: "#ae2012",
-    tgtClr: "#9b2226",
-  },
-  {
-    nbr: 512,
-    fromClr: "#9b2226",
-    tgtClr: "#5f0f40",
-  },
-  {
-    nbr: 1024,
-    fromClr: "#5f0f40",
-    tgtClr: "#001219",
-  },
-];
+// class NumTileCol {
+//   constructor(numTile, srcColumn, tgtColumn) {
+//     this.numTile = numTile;
+//     this.srcNColumn = srcColumn;
+//     this.tgtColumn = tgtColumn;
+//   }
 
-// .numberTwo {
-//     background-color: #e9d8a6;
-//     color: rgb(0, 0, 0);
+//   genRdmNbr() {
+//     // Function to generate the random number
+//     const numberRoll = [2, 4, 8, 16, 32, 64, 128];
+//     console.log("number roll is " + numberRoll);
+//     const getRandomNumberRoll = Math.floor(Math.random() * numberRoll.length);
+//     this.numTile = numberRoll[getRandomNumberRoll];
+//     // const rdmNumber = numberRoll[getRandomNumberRoll];
+//     console.log("rdmnumber is " + this.numTile);
 //   }
-//   .numberFour {
-//     background-color: #ee9b00;
-//     color: rgb(0, 0, 0);
+
+//   moveRdmNbr() {
+//     // Function to move the number down the column until it reaches base.
+//     console.log("Function to move random number");
 //   }
-//   .numberEight {
-//     background-color: #94d2bd;
-//     color: rgb(0, 0, 0);
+
+//   moveRdmNbrOnClick() {
+//     // Move the number to the column of click.
+//     console.log("Function to move random number on click");
 //   }
-//   .numbersSixteen {
-//     background-color: #ca6702;
+
+//   addToArray() {
+//     // Add the moved number to the array.
+//     console.log("Function to add random number to array");
 //   }
-//   .numbersThirtyTwo {
-//     background-color: #0a9396;
+
+//   addVertical() {
+//     // Function to add numbers that are same vertically.
+//     console.log("Function to add numbers vertically alligned");
 //   }
-//   .numbersSixtyFour {
-//     background-color: #bb3e03;
+
+//   addHorizontal() {
+//     // Function to add numbers that are same horizontally
+//     console.log("Function to add numbers horizontally alligned");
 //   }
-//   .numbersOneTwoEight {
-//     background-color: #005f73;
-//   }
-//   .numbersTwoFiveSix {
-//     background-color: #ae2012;
-//   }
-//   .numbersFiveOneTwo {
-//     background-color: #9b2226;
-//   }
-//   .numbersOneZTwoFour {
-//     background-color: #5f0f40;
-//   }
-//   .numbersTwoZFourEIght {
-//     background-color: #001219;
-//     color: rgb(255, 255, 255);
+// }
+
+//============== DECLARATIONS ============================
+
 const getModCont = document.querySelector(".modal-cont");
 const getModalContent = document.querySelector(".modalContent");
 const getModalBtn = document.querySelector(".modCont");
@@ -129,6 +132,14 @@ const getGameRulesModalCloseBtn = document.querySelector(
   ".closeModalGameRules"
 );
 const startGame = document.querySelector(".loadGame");
+const getColOne = document.querySelector(".colOne");
+const getColTwo = document.querySelector(".colTwo");
+const getColThree = document.querySelector(".colThree");
+const getColFour = document.querySelector(".colFour");
+const getColFive = document.querySelector(".colFive");
+const getColSix = document.querySelector(".colSix");
+
+//============== FUNCTION CALLS ============================
 
 function dispAboutGameModal() {
   console.log("Inside the About the game modal call");
@@ -153,13 +164,6 @@ function closeModalForAboutGame() {
   getAboutGameModal.style.display = "none";
   getAboutGameModalContent.style.display = "none";
 }
-function nxtNumberTile() {
-  let getRandomNumberRoll = Math.floor(Math.random() * numberRoll.length);
-  const rdmNumber = numberRoll[getRandomNumberRoll];
-  console.log("Next number is " + rdmNumber);
-  const getNxtTile = document.querySelector(".nextTile");
-  getNxtTile.textContent = "COMING UP... " + rdmNumber;
-}
 
 function closeLoadModal() {
   console.log("Inside the load game function");
@@ -174,8 +178,29 @@ function loadGame() {
   nxtNumberTile();
   console.log("Game landing page is on");
 }
+
+// const colmnOne = [];
+// const colmnTwo = [];
+// const colmnThree = [];
+// const colmnFour = [];
+// const colmnFive = [];
+// const colmnSix = [];
+
+// const getRandomNumberRoll = Math.floor(Math.random() * numberRoll.length);
+// const rdmNumber = numberRoll[getRandomNumberRoll];
+// console.log("rdmnumber is " + rdmNumber);
+
+function nxtNumberTile() {
+  const getNxtTile = document.querySelector(".nextTile");
+  getNxtTile.textContent = "COMING UP..." + rdmNumber;
+  // colmnOne.push(+rdmNumber);
+  // console.log(colmnOne);
+}
+
+//============== EVENT LISTENERS ============================
+
 getModalBtn.addEventListener("click", closeLoadModal);
-startGame.addEventListener("click", loadGame);
+startGame.addEventListener("click", startGame);
 getAboutGameBtn.addEventListener("click", dispAboutGameModal);
 getAboutGameModalCloseBtn.addEventListener("click", closeModalForAboutGame);
 
